3cb9f1e108adbfcd15dcc2f3b69c1bafc8ce0bed
Joakim Bomelin <joakim.bomelin@schibsted.com>
2022-01-12 09:48:57 +0100

    Add animation to scroller

--- scroller/scroller.c
+++ scroller/scroller.c
@@ -2,61 +2,63 @@
 
 int main() {
     struct bitmap font;
-    const char text[] = "YEAH !!  QUE PASA TIOS !!  ITS GREAT TO BE BACK HERE IN BARCELONA TONIGHT !!  1 2 3 4  !!  \0";
-    int i, x, y;
-    int xoffset, yoffset;
-    int ch, col;
+    struct bitmap image;
+    const char text[] = "IT S PEANUT BUTTER JELLY TIME!  2022 WILL MARK THE 20 TH ANNIVERSARY OF THIS GLORIOUS DANCING BANANA BUSTING ITS MOVES TO THE AMAZING TUNE  PEANUT BUTTER JELLY TIME  BY THE BUCKWHEAT BOYZ   NEVER FORGET!                   \0";
+    unsigned int offset, y, count;
+    unsigned char *buff;
+
+    if ((buff = malloc(320*200)) == NULL) {
+        printf("Unable to allocate memory for double buffer.\n");
+        exit(1);
+    }
+    memset(buff, 0, 320*200);
 
     font = read_bmp("font.bmp");
+    free(font.palette);
+
+    image = read_bmp("banana.bmp");
 
     set_mode(0x13);
-    set_palette(font.palette);
 
-/*
-    draw_bmp(&image, 0, 0);
-    wait(18);
+    set_palette(image.palette);
 
-    for (i = 0; i < 320; i++) {
-        for (y = 51; y < 100; y++) {
-            memcpy(&vga[y*320+81], &vga[y*320+82], 159);
-            vga[y*320+239] = 180;
-        }
-        wait_for_retrace();
-        wait_for_retrace();
-    }
-
-    wait(18);
-*/
-    ch = 0;
-    col = 0;
+    count = 0;
     while (!kbhit()) {
-        xoffset = (text[ch]-32)*8+col;
-
-        for (y = 96; y < 104; y++) {
-            memcpy(&vga[y*320], &vga[y*320+1], 320);
-            vga[y*320+319] = font.data[(y-96)*font.width+xoffset];
-
-            if (col == 7) {
-                memcpy(&vga[y*320], &vga[y*320+1], 320);
-                vga[y*320+319] = 0;
+        /* Image */
+        if (count%6 == 0) {
+            offset = ((count/6)%8)*80;
+            for (y = 0; y < 80; y++) {
+                memcpy(&buff[(y+52)*320+120], &image.data[y*640+offset], 80);
             }
         }
 
-        col++;
-        if (col > 7) {
-            ch++;
-            col = 0;
-        }
-        if (ch == strlen(text))
-            ch = 0;
+        /* Scroller */
+        offset = (text[(count/8)%strlen(text)]-32)*8+(count%8);
+        for (y = 140; y < 148; y++) {
+            memcpy(&buff[y*320+80], &buff[y*320+81], 160);
+            buff[y*320+240] = font.data[(y-140)*font.width+offset];
 
+            if (count%8 == 7) {
+                memcpy(&buff[y*320+80], &buff[y*320+81], 160);
+                buff[y*320+240] = 0;
+            }
+        }
+
+        /* Draw */
         wait_for_retrace();
+        memcpy(vga, buff, 320*200);
+
+        /* Count stuff */
+        count++;
     }
 
     set_mode(0x03);
 
-    free(font.palette);
     free(font.data);
+    free(image.palette);
+    free(image.data);
+
+    free(buff);
 
     return 0;
 }
